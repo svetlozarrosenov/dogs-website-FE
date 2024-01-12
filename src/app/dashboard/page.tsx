21ed54d2e37f913c9008998bf7e90ca2b7beb069
ssimeonov <svetlozarrosenov@gmail.com>
2024-01-12 18:35:58 +0200

    successfully redirect using server side rendering

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,20 @@
-'use client';
 import Sidebar from "@/components/dashboard/sidebar";
-import useLoginGuard from "@/guards/LoginGuard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from 'next/navigation';
 
-function Dashboard() {
-    useLoginGuard();
-    
+const Dashboard = async () => {
+    const session = await getServerSession(authOptions)
+
+    if ( !session) {
+        redirect('/login');
+    }
+
     return (
         <div className="dashboard">
             <Sidebar />
         </div>
     )
 }
+
 export default Dashboard;
